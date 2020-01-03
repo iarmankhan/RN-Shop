0ebb3dbbd942670be771d5491d5f5e472e0a56b2
iarmankhan <armankhan9244@gmail.com>
2020-01-03 14:58:32 +0530

    Navigator setup

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import {FlatList, StyleSheet} from "react-native";
+import {FlatList, StyleSheet, Text} from "react-native";
+import {useSelector} from "react-redux";
 
 const ProductOverviewScreen = props => {
+    const products = useSelector(state => state.products.availableProducts);
     return (
-      <FlatList data={} renderItem={} />
+        <FlatList data={products} renderItem={itemData => <Text>{itemData.item.title}</Text>} />
     );
+};
+
+ProductOverviewScreen.navigationOptions = {
+    headerTitle: 'All Products'
 };
 
 const styles = StyleSheet.create({
