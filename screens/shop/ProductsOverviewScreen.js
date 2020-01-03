6b209a6e69e078c6aff6e275ee1f0beabbcd00d4
iarmankhan <armankhan9244@gmail.com>
2020-01-03 13:00:02 +0530

    Product Overview Screen settings
    -Added dummy products
    -Created reducer for products
    -Initialized store in App.js

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {FlatList, StyleSheet} from "react-native";
+
+const ProductOverviewScreen = props => {
+    return (
+      <FlatList data={} renderItem={} />
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default ProductOverviewScreen
