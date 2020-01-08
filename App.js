1ff0e290f7ddd572c5a9ecab5c4d1f048faeb9b4
iarmankhan <armankhan9244@gmail.com>
2020-01-08 12:20:28 +0530

    Installed ReduxThunk and set up it in App.js

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import {Provider} from "react-redux";
 import * as Fonts from 'expo-font'
 import {AppLoading} from "expo";
+import ReduxThunk from 'redux-thunk';
 
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
@@ -16,7 +17,7 @@ const rootReducer = combineReducers({
     orders: ordersReducer
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 const fetchFonts = () => {
     return Fonts.loadAsync({
